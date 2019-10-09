ff9af4c26f669d16306e0b280088fb8a06f6210f
b166erobot <b166erobot@outlook.com>
2019-10-08 21:12:03 -0300

    exercício resolvido.

--- modulo.py
+++
@@ -1,9 +0,0 @@
-def func(pedras: str, joia: str) -> int:
-    """ Função que retorna quantas strings pedras contém na joia pedra. """
-    quantidade = sum([joia.count(pedra) for pedra in pedras])
-    return quantidade
-
-
-if __name__ == '__main__':
-    print(func(pedras='aA', joia='aAAbbbb'))
-    print(func(pedras='z', joia='ZZ'))
--- src/modulo.py
+++ src/modulo.py
@@ -1,2 +1,4 @@
-def func(pedras="", joia=""):
-    # Escreva seu codigo aqui
+def func(pedras: str = "", joia: str = "") -> int:
+    """ Função que retorna quantas strings pedras contém na joia pedra. """
+    quantidade = sum([joia.count(pedra) for pedra in pedras])
+    return quantidade
